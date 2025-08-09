0522d64cb9d5e5ab4e1548e10872abc05031e39d
saihgupr <saihgupr@duck.com>
2025-08-09 12:09:15 -0400

    Initial commit

---
+++ Frigate Events/SettingsStore.swift
@@ -0,0 +1,54 @@
+
+import Foundation
+import Combine
+
+class SettingsStore: ObservableObject {
+    @Published var frigateBaseURL: String {
+        didSet {
+            UserDefaults.standard.set(frigateBaseURL, forKey: "frigateBaseURL")
+        }
+    }
+
+    @Published var availableLabels: [String] = []
+    @Published var selectedLabels: Set<String> {
+        didSet {
+            UserDefaults.standard.set(Array(selectedLabels), forKey: "selectedLabels")
+        }
+    }
+
+    @Published var availableZones: [String] = []
+    @Published var selectedZones: Set<String> {
+        didSet {
+            UserDefaults.standard.set(Array(selectedZones), forKey: "selectedZones")
+        }
+    }
+
+    @Published var availableCameras: [String] = []
+    @Published var selectedCameras: Set<String> {
+        didSet {
+            UserDefaults.standard.set(Array(selectedCameras), forKey: "selectedCameras")
+        }
+    }
+
+    init() {
+        self.frigateBaseURL = UserDefaults.standard.string(forKey: "frigateBaseURL") ?? "http://192.168.1.168:5000"
+        
+        if let savedLabels = UserDefaults.standard.array(forKey: "selectedLabels") as? [String] {
+            self.selectedLabels = Set(savedLabels)
+        } else {
+            self.selectedLabels = []
+        }
+
+        if let savedZones = UserDefaults.standard.array(forKey: "selectedZones") as? [String] {
+            self.selectedZones = Set(savedZones)
+        } else {
+            self.selectedZones = []
+        }
+
+        if let savedCameras = UserDefaults.standard.array(forKey: "selectedCameras") as? [String] {
+            self.selectedCameras = Set(savedCameras)
+        } else {
+            self.selectedCameras = []
+        }
+    }
+}
